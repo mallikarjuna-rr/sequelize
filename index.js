835508fc91e09b35b4ed72482bbfb4dac8c201d3
unknown <mallikarjunreddy.rr@gmail.com>
2022-10-22 15:50:03 +0530

    added conection with Database

---
+++ index.js
@@ -0,0 +1,30 @@
+const Sequelize = require('sequelize');
+
+// Connection Check with async and await
+const sequelize = async () => {
+    const connection = new Sequelize('sequelize_node', 'root', 'Malli@123', {
+        dialect: 'mysql'
+    });
+
+    try {
+        await connection.authenticate();
+        console.info("Database Connection SUCCESS")
+    }catch(err) {
+        console.error("Database Connection FAILURE",err.msg)
+    }
+};
+
+sequelize();
+
+module.exports = sequelize;
+
+// Connection Check with Promises
+// const sequelize = new Sequelize('sequelize_node', 'root', 'Malli@123', {
+//     dialect: 'mysql'
+// });
+
+// sequelize.authenticate().then(() => {
+//     console.info("Database Connection SUCCESS")
+// }).catch((err) => {
+//     console.error("Database Connection FAILURE",err.msg)
+// });
