deed0560db8c0ebe9ea954333f392cda44b95206
unknown <mallikarjunreddy.rr@gmail.com>
2022-10-23 11:11:23 +0530

    user model added

---
+++ models/user_model.js
@@ -0,0 +1,35 @@
+const sequelize = require('../index');
+const { DataTypes, Model } = require('sequelize');
+
+
+const User = sequelize.define('user', {
+    user_id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    userName: {
+        type: DataTypes.STRING,
+        unique: true
+    },
+    passWord: {
+        type: DataTypes.STRING
+    },
+    age: {
+        type: DataTypes.INTEGER
+    },
+    isActive: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+    }
+});
+
+User.sync({alter: true }).then(() => {
+    console.log("Table Created")
+}).catch((err) => {
+    console.log("Table not Created")
+})
