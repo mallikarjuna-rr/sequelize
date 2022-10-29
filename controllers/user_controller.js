ac63309da54e5c2248406ee162a4845988812c45
mallikarjuna-rr <mallikarjunreddy.rr@gmail.com>
2022-10-29 20:30:48 +0530

    added finder methods

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,7 +1,7 @@
 const User = require('../models/user_model');
 const sequelize = require('../index');
-const { Op} = require('sequelize')
-const createUser = async() => {
+const { Op } = require('sequelize')
+const createUser = async () => {
     return await User.create({
         name: "Mallikarjuna",
         userName: "Malli",
@@ -10,12 +10,12 @@ const createUser = async() => {
     })
 };
 
-const bulkUserCreate = async() => {
+const bulkUserCreate = async () => {
     return await User.bulkCreate([
         {
             name: "Vishnu",
             age: 26
-        }, 
+        },
         {
             name: "Siva",
             age: "28"
@@ -24,7 +24,7 @@ const bulkUserCreate = async() => {
     ])
 };
 
-const ageIncrement = async() => {
+const ageIncrement = async () => {
     const age = await User.create({
         name: "KArthik",
         age: 5
@@ -33,7 +33,7 @@ const ageIncrement = async() => {
     return await age.save()
 };
 
-const findAllUsers = async() => {
+const findAllUsers = async () => {
     // const data =  await User.findAll();
     // const data = await User.findAll({ limit : 2})
     // const data = await User.findAll({ attributes: ['name','passWord']}) // findout with name and passWord only
@@ -57,26 +57,57 @@ const findAllUsers = async() => {
     // const data = await User.findAll({ where: {age: { [Op.or]: { [Op.lt] : 40, [Op.eq]: null}}}})
     // const data = await User.findAll({ where: sequelize.where(sequelize.fn('char_length', sequelize.col('name')), 6)})
     // const data = await User.sum('age')
-    const data = await User.sum('age', {where: {age:5}})
-    console.log("data..",data)
+    const data = await User.sum('age', { where: { age: 5 } })
+    console.log("data..", data)
     // data.forEach((ele) => {
     //     console.log(ele.toJSON())
     // });  
 }
 
-const updateUser = async() => {
+const finderMethod = async () => {
+    // const data = await User.findByPk(1,{raw:true})
+    // const data = await User.findOne({raw:true})
+    // const data = await User.findOne({
+    //     where: {
+    //         age: {
+    //             [Op.or]: {
+    //                 [Op.lt]: 6,
+    //                 [Op.eq]: null
+    //             }
+    //         }
+    //     },
+    //     raw: true
+    // })
+    // const data = await User.findOrCreate({ where: {name: 'Gowtham'},raw:true})
+    // const data = await User.findOrCreate({where: {name: 'S',isActive:true}})
+    // const [result, created ] = data; // here we wil console created(true/false) or result....
+    // console.log("data...", created)
+    // console.log("data...",result)
+    const data = await User.findAndCountAll({
+        where: { userName: 'Malli'},
+        raw: true // gives JSON type
+    })
+    const { count, rows} = data;
+    console.log("count...",count)
+    console.log("rows...",rows)
+    // return data
+}
+
+const updateUser = async () => {
     // const data = await User.update({userName : 'Kannayya'}, {where: { name:'Siva'}})
-    const data = await User.update({ passWord: 'KARTHIK'}, {where: { age: {[Op.gt]: 5 }}})
+    const data = await User.update({ passWord: 'KARTHIK' }, { where: { age: { [Op.gt]: 5 } } })
     return data;
 }
 
-const deleteUser = async() => {
-    const data = await User.destroy({ where: {name:'Nandu'}})
+const deleteUser = async () => {
+    const data = await User.destroy({ where: { name: 'Nandu' } })
     return data
 }
 // bulkUserCreate();
 // ageIncrement()
 // createUser(); 
-findAllUsers()
+// findAllUsers()
+finderMethod()
 // updateUser()
 // deleteUser()
+
